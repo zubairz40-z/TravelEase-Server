9cbf14748ee5700e983a522178ae6a8084fe782d
zubairz40-z <zahirzubair740@gmail.com>
2025-11-30 21:37:00 +0600

    changed few stuff for deployment

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     console.log("✅ Connected to MongoDB");
 
     const db = client.db(dbName);
